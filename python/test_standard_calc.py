9ee63cce364a99808e7b27d70cea804ddd8710aa
akash-venkateshwaran <akash.venkateshwaran@gmail.com>
2023-09-20 15:27:18 -0700

    More unit tests

--- python/test_standard_calc.py
+++ python/test_standard_calc.py
@@ -1,54 +1,71 @@
 from standard_calc import bound_to_180, is_angle_between
-import pytest
+
 
 """ Tests for bound_to_180() """
+
 
 def test_bound_basic1():
     assert bound_to_180(0) == 0
 
+
 def test_bound_basic2():
     assert bound_to_180(360) == 0
+
 
 def test_bound_basic3():
     assert bound_to_180(180) == -180
 
+
 def test_bound_basic4():
     assert bound_to_180(-180) == -180
+
 
 def test_bound_basic5():
     assert bound_to_180(90) == 90
 
+
 def test_bound_basic6():
     assert bound_to_180(-90) == -90
+
 
 def test_bound_basic7():
     assert bound_to_180(990) == -90
 
+
 """ Tests for is_angle_between() """
+
 
 def test_between_basic1():
     assert is_angle_between(0, 1, 2) == True
 
+
 def test_between_basic2():
     assert is_angle_between(10, 10, 2) == True
+
 
 def test_between_inv1():
     assert is_angle_between(10, 1, -20) == True
 
+
 def test_between_inv2():
     assert is_angle_between(-20, 1, 10) == True
+
 
 def test_between_ext1():
     assert is_angle_between(90, 1, -90) == True
 
+
 def test_between_ext2():
     assert is_angle_between(0, -90 , -180) == True
+
 
 def test_between_ext3():
     assert is_angle_between(0, 90 , 180) == False
 
+
 def test_between_ext4():
     assert is_angle_between(120, 180 , -120) == True
+
 
 def test_between_ext5():
     assert is_angle_between(180, 0 , -180) == False
